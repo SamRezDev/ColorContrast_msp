49095d4c9d90fd3134d4b0efcf94a8f06360e134
Gawsty Seri <sgtghostbo@gmail.com>
2021-09-06 00:57:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState,useEffect } from "react";
+import TileContainer from "./container/TileContainer";
 function App() {
+  const [ContainerArray, setContainerArray] = useState([]);
+  const [AddElement, setAddElement] = useState()
+  useEffect(() => {
+    AddContainer()
+  }, [AddElement])
+function AddContainer(){
+  var uniqid = Date.now();
+  var array= ContainerArray ;
+  array.push({id :uniqid})
+  setContainerArray(array)
+  console.log(ContainerArray)
+}
+function RemoveContainer(IdRemoved){
+ setContainerArray( ContainerArray.filter((element) => {
+    return IdRemoved !== element.id;
+  }))
+  console.log(ContainerArray)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='AddContainerButton' onClick={() => AddElement?setAddElement(false):setAddElement(true)}> +</button>
+      {ContainerArray.map( (element) => {return <TileContainer key={element.id} id={element.id} RemoveContainer={RemoveContainer} />} ) }
+
+     
     </div>
   );
 }
